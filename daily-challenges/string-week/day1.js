b534861c9ec7b73daa264a3f14bdf7d5002a2fb7
siriusscaper <siriusscaper@gmail.com>
2022-09-22 17:24:19 -0700

    new: examples for interviews

--- daily-challenges/string-week/day1.js
+++ daily-challenges/string-week/day1.js
@@ -1,14 +1,14 @@
-// '0' <-> 'O'      '5' <-> 'S'
-// '1' <-> 'I'      '6' <-> 'G'
-// '2' <-> 'Z'      '7' <-> 'L'
-// '3' <-> 'E'      '8' <-> 'B'
-// '4' <-> 'h'      '9' <-> 'q'
+//?  '0' <-> 'O'      '5' <-> 'S'
+//?  '1' <-> 'I'      '6' <-> 'G'
+//?  '2' <-> 'Z'      '7' <-> 'L'
+//?  '3' <-> 'E'      '8' <-> 'B'
+//?  '4' <-> 'h'      '9' <-> 'q'
 
-// "PR0-T1P #hqB: 1T'5 N1C3 T0 5AY H3770." 
-// implement fix function -> 
-// "PRO-TIP #498: IT'S NICE TO SAY HELLO."
+//* "PR0-T1P #hqB: 1T'5 N1C3 T0 5AY H3770." 
+//* implement fix function -> 
+//* "PRO-TIP 498: IT'S NICE TO SAY HELLO."
 
-// "e!!Igv)t5lltBcvbdeDH3dVw!OOtI#Aa.ZMDu7WYpP^VVjDc4I50iv#ylhgmQfs"
+//+ "e!!Igv)t5lltBcvbdeDH3dVw!OOtI#Aa.ZMDu7WYpP^VVjDc4I50iv#ylhgmQfs"
 
 // Parameters
 
---
+++ huntober-classes/prep-practice.js
@@ -0,0 +1,3 @@
+//+ Code a function that says whether a word is a palindrome
+
+//? We are taking a word
