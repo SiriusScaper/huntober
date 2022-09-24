5e0825be70c5c4cf4599372b64366ba343fdf984
siriusscaper <siriusscaper@gmail.com>
2022-09-23 17:31:18 -0700

    feat: working through PREP and how to apply it to interviews and keep them engaged and communicate welll

--- huntober-classes/prep-practice.js
+++ huntober-classes/prep-practice.js
@@ -1,3 +1,15 @@
 //+ Code a function that says whether a word is a palindrome
 
-//? We are taking a word
+//? word => str, no empty str, no !, no nums, no funny biz, no caps, 1 word
+//? return true || false
+//? racecar => true
+//? bob => true
+//? tacos => false
+
+function isPalindrome(word){
+  
+}
+
+console.log(isPlaindrome('racecar'), true)
+console.log(isPlaindrome('bob'), true)
+console.log(isPlaindrome('tacos'), false)
