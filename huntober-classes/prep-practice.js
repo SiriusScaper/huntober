874837f02dbe300467e0d04cf0d3fb42d51f7866
siriusscaper <siriusscaper@gmail.com>
2022-09-24 18:11:12 -0700

    feat: Added more comments and answer to test tech interview

--- huntober-classes/prep-practice.js
+++ huntober-classes/prep-practice.js
@@ -1,15 +1,29 @@
 //+ Code a function that says whether a word is a palindrome
 
+
+//+ So 
 //? word => str, no empty str, no !, no nums, no funny biz, no caps, 1 word
+
 //? return true || false
+
 //? racecar => true
 //? bob => true
 //? tacos => false
 
-function isPalindrome(word){
-  
-}
+const isPalindrome = word => word === word.split('').reverse().join('')
 
-console.log(isPlaindrome('racecar'), true)
-console.log(isPlaindrome('bob'), true)
-console.log(isPlaindrome('tacos'), false)
+// function isPalindrome(word){
+  //? word => reverse the word - split, reverse, join
+//   let reversedWord = word.split('').reverse().join('')
+  //? revWord === word ? true : false
+//   return reversedWord === word
+// }
+
+
+
+
+console.log(isPalindrome('racecar'), true)
+console.log(isPalindrome('bob'), true)
+console.log(isPalindrome('tacos'), false)
+console.log(isPalindrome('nathan'), false)
+console.log(isPalindrome('natan'), true)
