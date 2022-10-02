0b4c18b03c7eb629ff167db89ad7fc296e6fd30e
siriusscaper <siriusscaper@gmail.com>
2022-10-01 18:22:09 -0700

    working on challenges

---
+++ daily-challenges/string-week/day1.js
@@ -0,0 +1,20 @@
+// '0' <-> 'O'      '5' <-> 'S'
+// '1' <-> 'I'      '6' <-> 'G'
+// '2' <-> 'Z'      '7' <-> 'L'
+// '3' <-> 'E'      '8' <-> 'B'
+// '4' <-> 'h'      '9' <-> 'q'
+
+// "PR0-T1P #hqB: 1T'5 N1C3 T0 5AY H3770." 
+// implement fix function -> 
+// "PRO-TIP #498: IT'S NICE TO SAY HELLO."
+
+// Parameters
+
+
+// Returns
+
+
+// Examples
+
+
+// Pseudo Code
