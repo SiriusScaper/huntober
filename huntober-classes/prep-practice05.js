8ebe41ddfa36f6ddec961bd3152d8f04e3f97f78
siriusscaper <siriusscaper@gmail.com>
2022-10-08 18:42:55 -0700

    Add: Prep from previous days

--- huntober-classes/prep-practice05.js
+++ huntober-classes/prep-practice05.js
@@ -5,3 +5,14 @@
 // assert.strictEqual(sumMix([9, 3, '7', '3']), 22);
 // assert.strictEqual(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]), 42); 
 // assert.strictEqual(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']), 41); 
+
+//+ Parameters
+
+
+//+ Returns
+
+
+//+ Examples
+
+
+//+ Pseudo Code
