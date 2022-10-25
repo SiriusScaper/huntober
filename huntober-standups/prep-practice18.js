cb2c221ef59feeb16579e0f705a5daa8bd80b398
siriusscaper <siriusscaper@gmail.com>
2022-10-25 15:08:46 -0700

    Added another challenge

---
+++ huntober-standups/prep-practice18.js
@@ -0,0 +1,16 @@
+// Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. For example:
+
+// "alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+
+// --> "alpha beta gamma delta alpha beta gamma delta"
+
+//+ Parameters
+
+
+//+ Returns
+
+
+//+ Examples
+
+
+//+ Pseudo Code
